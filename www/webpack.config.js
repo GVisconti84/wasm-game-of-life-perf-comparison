5446ce1c9a37848ffa860b0e051b285af70862d1
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2020-04-09 19:03:16 +0200

    [WWW] Introduce TypeScript support.

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -2,12 +2,26 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: "./bootstrap.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
   },
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    // IMPORTANT! This is the list of extensions webpack will try to match when
+    // importing from ts files. We need to add '.wasm' because of the "wasm_game_of_life_bg" import!
+    extensions: [ '.tsx', '.ts', '.js', '.wasm' ],
+  },
   plugins: [
     new CopyWebpackPlugin(['index.html'])
   ],
