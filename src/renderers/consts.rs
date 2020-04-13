21b199aab8c82027944a551bab7e826732295a7d
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2020-04-13 17:26:38 +0200

    Cleanup

--- src/renderers/consts.rs
+++ src/renderers/consts.rs
@@ -1,7 +1,7 @@
-pub const BPP: usize = 4;
 // TODO: Can we centralize these constants between Js and Rs?
+pub const BPP: usize = 4;
 pub const CELL_SIZE:  usize = 5;
 pub const GRID_SIZE:  usize = 1;
-pub const GRID_COLOR:  &'static [u8] = &[204, 204, 204, 255];  // TODO: we might be able to optimize out the alpha channel
+pub const GRID_COLOR:  &'static [u8] = &[204, 204, 204, 255];
 pub const DEAD_COLOR:  &'static [u8] = &[255, 255, 255, 255];
 pub const ALIVE_COLOR: &'static [u8] = &[ 22,  22,  22, 255];
