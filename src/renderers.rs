197558613820e1ae13cb7d76202e84faf39dd645
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2020-04-13 12:21:51 +0200

    [CORE] Cleanup module names.

--- src/renderers.rs
+++ src/renderers.rs
@@ -1,29 +1,29 @@
 use wasm_bindgen::prelude::*;
 use crate::universe::RsUniverse;
 
-mod renderer;
-mod dummyrendererfloat;
-mod renderer1;
-mod renderer2;
+mod rs_renderer;
+mod rs_dummy_renderer_float;
+mod rs_renderer_1;
+mod rs_renderer_2;
 
 pub mod consts;
-pub use renderer::*;
+pub use rs_renderer::*;
 
 
 #[wasm_bindgen(js_name = dummyRenderFloat)]
 pub fn dummy_render_float(r: &mut RsRenderer, u: &RsUniverse) {
-    use dummyrendererfloat::*;
+    use rs_dummy_renderer_float::*;
     r.render(u);
 }
 
 #[wasm_bindgen]
 pub fn render1(r: &mut RsRenderer, u: &RsUniverse) {
-    use renderer1::*;
+    use rs_renderer_1::*;
     r.render(u);
 }
 
 #[wasm_bindgen]
 pub fn render2(r: &mut RsRenderer, u: &RsUniverse) {
-    use renderer2::*;
+    use rs_renderer_2::*;
     r.render(u);
 }
